70a86a11bad81af4372831b2c0fbe0e109bedd16
Andres Kasekamp <andreskasekamp@gmail.com>
2023-08-21 16:13:09 +0300

    Memory fix

--- app.js
+++ app.js
@@ -213,7 +213,6 @@ require([
     /**************************************
      * Perfomance info
      **************************************/
-    // TODO modulariseeri
 
     let isPerformanceInfoVisible = false;
     const performanceMeasureBtn = document.getElementById(
@@ -228,7 +227,7 @@ require([
       if (isPerformanceInfoVisible) {
         performanceMeasureInfo.style.display = "none";
       } else {
-        updatePerformanceInfo()
+        updatePerformanceInfo();
         performanceMeasureInfo.style.display = "inline";
         view.ui.add(performanceMeasureInfo, "top-right");
       }
@@ -239,74 +238,13 @@ require([
 
   const updatePerformanceInfo = () => {
     const performanceInfo = view.performanceInfo;
-    updateMemoryTitle(
+
+    initMemoryTest.updateMemoryTitle(
       performanceInfo.usedMemory,
       performanceInfo.totalMemory,
       performanceInfo.quality
     );
-    updateTables(performanceInfo);
+    initMemoryTest.updateTables(performanceInfo);
     setTimeout(updatePerformanceInfo, 1000);
   };
-
-  function updateMemoryTitle(used, total, quality) {
-    const title = document.getElementById("title");
-    title.innerHTML = `Memory: ${getMB(used)}MB/${getMB(
-      total
-    )}MB  -  Quality: ${Math.round(100 * quality)} %`;
-  }
-
-  function updateTables(stats) {
-    const tableMemoryContainer = document.getElementById("memory");
-    const tableCountContainer = document.getElementById("count");
-    tableMemoryContainer.innerHTML = `<tr>
-      <th>Resource</th>
-      <th>Memory(MB)</th>
-    </tr>`;
-    for (let layerInfo of stats.layerPerformanceInfos) {
-      if (layerInfo.memory !== 0) {
-        const row = document.createElement("tr");
-        row.innerHTML = `<td>${
-          layerInfo.layer.title
-        }</td><td class="center">${getMB(layerInfo.memory)}</td>`;
-        tableMemoryContainer.appendChild(row);
-      }
-    }
-
-    tableCountContainer.innerHTML = `<tr>
-      <th>Layer - Features</th>
-      <th>Displayed / Max<br>(count)</th>
-      <th>Total<br>(count)</th>
-    </tr>`;
-
-    for (let layerInfo of stats.layerPerformanceInfos) {
-      if (
-        layerInfo.maximumNumberOfFeatures &&
-        layerInfo.displayedNumberOfFeatures !== 0
-      ) {
-        const row = document.createElement("tr");
-        row.innerHTML = `<td>${layerInfo.layer.title}`;
-        row.innerHTML += `<td class="center">${
-          layerInfo.displayedNumberOfFeatures
-            ? layerInfo.displayedNumberOfFeatures
-            : "-"
-        } / ${
-          layerInfo.maximumNumberOfFeatures
-            ? layerInfo.maximumNumberOfFeatures
-            : "-"
-        }</td>`;
-        row.innerHTML += `<td class="center">${
-          layerInfo.totalNumberOfFeatures
-            ? layerInfo.totalNumberOfFeatures
-            : "-"
-        }</td>`;
-        tableCountContainer.appendChild(row);
-      }
-    }
-  }
-
-  function getMB(bytes) {
-    const kilobyte = 1024;
-    const megabyte = kilobyte * 1024;
-    return Math.round(bytes / megabyte);
-  }
 });
